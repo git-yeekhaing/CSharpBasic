6311a9caa6c6ac0e1ee599de4727800abe942628
myanmmar-polestar <mm.coursefinder@gmail.com>
2020-06-07 17:53:44 +0900

    IEnumerable and IQuerable

--- CSharpBasic/Program.cs
+++ CSharpBasic/Program.cs
@@ -7,67 +7,82 @@ using System.Threading.Tasks;
 
 namespace CSharpBasic
 {
+    //Example: We have an integer list and we need to write a LINQ query 
+    //which will return all the integers which are greater than 5. We are going to create a console application.
     class Program
     {
-        
-
         static void Main(string[] args)
         {
-            
-        }     
+            IEnumerabelWithQuery();
+            IEnumerabelWithMixed();
+            IQueryableWithMixed();
+
+            Console.ReadKey();
+        }
+
+        public static void IQueryableWithMixed()
+        {
+            List<Student> studentList = new List<Student>()
+            {
+                new Student(){ID = 1, Name = "James", Gender = "Male"},
+                new Student(){ID = 2, Name = "Sara", Gender = "Female"},
+                new Student(){ID = 3, Name = "Steve", Gender = "Male"},
+                new Student(){ID = 4, Name = "Pam", Gender = "Female"}
+            };
+
+            //Linq Query to Fetch all students with Gender Male
+            IQueryable<Student> MethodSyntax = studentList.AsQueryable()
+                                .Where(std => std.Gender == "Male");
+
+            //Iterate through the collection
+            foreach (var student in MethodSyntax)
+            {
+                Console.WriteLine($"ID : {student.ID}  Name : {student.Name}");
+            }
+        }
+
+        public static void IEnumerabelWithMixed()
+        {
+            List<Student> studentList = new List<Student>()
+            {
+                new Student(){ID = 1, Name = "James", Gender = "Male"},
+                new Student(){ID = 2, Name = "Sara", Gender = "Female"},
+                new Student(){ID = 3, Name = "Steve", Gender = "Male"},
+                new Student(){ID = 4, Name = "Pam", Gender = "Female"}
+            };
+            //Linq Query to Fetch all students with Gender Male
+            IEnumerable<Student> QuerySyntax = from std in studentList
+                                               where std.Gender == "Male"
+                                               select std;
+            //Iterate through the collection
+            foreach (var student in QuerySyntax)
+            {
+                Console.WriteLine($"ID : {student.ID}  Name : {student.Name}");
+            }
+        }
+
+        public static void IEnumerabelWithQuery()
+        {
+            List<int> integerList = new List<int>()
+            {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+            };
+
+            IEnumerable<int> QuerySyntax = from obj in integerList
+                                           where obj > 5
+                                           select obj;
+
+            foreach (var item in QuerySyntax)
+            {
+                Console.Write(item + " ");
+            }
+        }
     }
 
-    public class Car
+    public class Student
     {
-        private string _CarName = "Honda City";
-        private string _CarColur = "Black";
-        public string CarName
-        {
-            set
-            {
-                _CarName = value;
-            }
-            get
-            {
-                return _CarName;
-            }
-        }
-        public string CarColur
-        {
-            set
-            {
-                _CarColur = value;
-            }
-            get
-            {
-                return _CarColur;
-            }
-        }
-        public void Steering()
-        {
-            Console.WriteLine("Streering of the Car");
-        }
-
-        public void Brakes()
-        {
-            Console.WriteLine("Brakes of the Car");
-        }
-        public void Gear()
-        {
-            Console.WriteLine("Gear of the Car");
-        }
-        private void CarEngine()
-        {
-            Console.WriteLine("Engine of the Car");
-        }
-        private void DiesalEngine()
-        {
-            Console.WriteLine("DiesalEngine of the Car");
-        }
-
-        private void Silencer()
-        {
-            Console.WriteLine("Silencer of the Car");
-        }
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
     }
 }
