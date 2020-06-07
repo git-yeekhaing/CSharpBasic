de37a3c0e178aeaed6323f75be58e6e7af67f16e
myanmmar-polestar <mm.coursefinder@gmail.com>
2020-06-07 12:34:55 +0900

    using generic

--- CSharpBasic/Program.cs
+++ CSharpBasic/Program.cs
@@ -10,33 +10,24 @@ namespace CSharpBasic
     {
         static void Main(string[] args)
         {
-            //bool IsEqual = ClsCalculator.AreEqual(10, 20);
-            //bool IsEqual = ClsCalculator.AreEqual("ABC", "ABC");
-            bool IsEqual = ClsCalculator.AreEqual(10.5, 20.5);
-            if (IsEqual)
-            {
-                Console.WriteLine("Both are Equal");
-            }
-            else
-            {
-                Console.WriteLine("Both are Not Equal");
-            }
+            bool IsEqual = ClsCalculator.AreEqual(10, 20);
+            Console.WriteLine($"int result {IsEqual}");
+
+            IsEqual = ClsCalculator.AreEqual("ABC", "ABC");
+            Console.WriteLine($"string result {IsEqual}");
+
+            IsEqual = ClsCalculator.AreEqual(10.5, 20.5);
+            Console.WriteLine($"double result {IsEqual}");
+            
             Console.ReadKey();
         }
     }
+
     public class ClsCalculator
     {
-        public static bool AreEqual(int value1, int value2)
+        public static bool AreEqual<T>(T value1, T value2)
         {
-            return value1 == value2;
-        }
-        public static bool AreEqual(string value1, string value2)
-        {
-            return value1 == value2;
-        }
-        public static bool AreEqual(double value1, double value2)
-        {
-            return value1 == value2;
-        }
+            return value1.Equals(value2);
+        }        
     }
 }
