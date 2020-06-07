bad6e11548faf8ab4e165c7bd68f5272a00c97c8
myanmmar-polestar <mm.coursefinder@gmail.com>
2020-06-07 16:15:36 +0900

    using abstration

--- CSharpBasic/Program.cs
+++ CSharpBasic/Program.cs
@@ -9,17 +9,65 @@ namespace CSharpBasic
 {
     class Program
     {
-        public delegate string GreetingsDelegate(string name);
+        
 
         static void Main(string[] args)
         {
-            GreetingsDelegate obj = delegate (string name)
+            
+        }     
+    }
+
+    public class Car
+    {
+        private string _CarName = "Honda City";
+        private string _CarColur = "Black";
+        public string CarName
+        {
+            set
             {
-                return "Hello @" + name + " welcome to Dotnet Tutorials";
-            };
-            string GreetingsMessage = obj.Invoke("Pranaya");
-            Console.WriteLine(GreetingsMessage);
-            Console.ReadKey();
+                _CarName = value;
+            }
+            get
+            {
+                return _CarName;
+            }
         }
-    }    
+        public string CarColur
+        {
+            set
+            {
+                _CarColur = value;
+            }
+            get
+            {
+                return _CarColur;
+            }
+        }
+        public void Steering()
+        {
+            Console.WriteLine("Streering of the Car");
+        }
+
+        public void Brakes()
+        {
+            Console.WriteLine("Brakes of the Car");
+        }
+        public void Gear()
+        {
+            Console.WriteLine("Gear of the Car");
+        }
+        private void CarEngine()
+        {
+            Console.WriteLine("Engine of the Car");
+        }
+        private void DiesalEngine()
+        {
+            Console.WriteLine("DiesalEngine of the Car");
+        }
+
+        private void Silencer()
+        {
+            Console.WriteLine("Silencer of the Car");
+        }
+    }
 }
