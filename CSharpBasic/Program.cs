247621972ec14ed1d299e67eaef7a0e1ffe73b33
myanmmar-polestar <mm.coursefinder@gmail.com>
2020-06-07 12:13:52 +0900

    why need generic reason 1

---
+++ CSharpBasic/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBasic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool IsEqual = ClsCalculator.AreEqual(10, 20);
+            if (IsEqual)
+            {
+                Console.WriteLine("Both are Equal");
+            }
+            else
+            {
+                Console.WriteLine("Both are Not Equal");
+            }
+            Console.ReadKey();
+        }
+    }
+    public class ClsCalculator
+    {
+        public static bool AreEqual(int value1, int value2)
+        {
+            return value1 == value2;
+        }
+    }
+}
