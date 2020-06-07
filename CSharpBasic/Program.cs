0aae0e060ff5385e607adffd0794df16821d3066
myanmmar-polestar <mm.coursefinder@gmail.com>
2020-06-07 14:21:15 +0900

    reason 1 (using delegate to bind a function)

--- CSharpBasic/Program.cs
+++ CSharpBasic/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,42 +9,18 @@ namespace CSharpBasic
 {
     class Program
     {
+        public delegate string GreetingsDelegate(string name);
+
         static void Main(string[] args)
         {
-            MyGenericClass<int> integerGenericClass = new MyGenericClass<int>(10);
-            int val = integerGenericClass.genericMethod(200);
-
-            Console.WriteLine();
-
-            MyGenericClass<string> strGenericClass = new MyGenericClass<string>("ABC");
-            string strval = strGenericClass.genericMethod("DEF");
-
+            GreetingsDelegate obj = new GreetingsDelegate(Greetings);
+            string GreetingsMessage = obj.Invoke("Pranaya");
+            Console.WriteLine(GreetingsMessage);
             Console.ReadKey();
         }
-    }
-
-    public class MyGenericClass<T>
-    {
-        //Generic variable
-        //The data type is generic
-        private T genericMemberVariable;
-        //Generic Constructor
-        //Constructor accepts one parameter of Generic type
-        public MyGenericClass(T value)
+        public static string Greetings(string name)
         {
-            genericMemberVariable = value;
+            return "Hello @" + name + " welcome to Dotnet Tutorials";
         }
-        //Generic Method
-        //Method accepts one Generic type Parameter
-        //Method return type also Generic
-        public T genericMethod(T genericParameter)
-        {
-            Console.WriteLine("Parameter type: {0}, value: {1}", typeof(T).ToString(), genericParameter);
-            Console.WriteLine("Return type: {0}, value: {1}", typeof(T).ToString(), genericMemberVariable);
-            return genericMemberVariable;
-        }
-        //Generic Property
-        //The data type is generic
-        public T genericProperty { get; set; }
-    }
+    }    
 }
