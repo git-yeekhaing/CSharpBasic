c7bffee0afe14bfd158cbbf2824678ce30173789
myanmmar-polestar <mm.coursefinder@gmail.com>
2020-06-07 12:45:26 +0900

    using generic class, variable, method, constructor, parameter

--- CSharpBasic/Program.cs
+++ CSharpBasic/Program.cs
@@ -10,24 +10,40 @@ namespace CSharpBasic
     {
         static void Main(string[] args)
         {
-            bool IsEqual = ClsCalculator.AreEqual(10, 20);
-            Console.WriteLine($"int result {IsEqual}");
+            MyGenericClass<int> integerGenericClass = new MyGenericClass<int>(10);
+            int val = integerGenericClass.genericMethod(200);
 
-            IsEqual = ClsCalculator.AreEqual("ABC", "ABC");
-            Console.WriteLine($"string result {IsEqual}");
+            Console.WriteLine();
 
-            IsEqual = ClsCalculator.AreEqual(10.5, 20.5);
-            Console.WriteLine($"double result {IsEqual}");
-            
+            MyGenericClass<string> strGenericClass = new MyGenericClass<string>("ABC");
+            string strval = strGenericClass.genericMethod("DEF");
+
             Console.ReadKey();
         }
     }
 
-    public class ClsCalculator
+    public class MyGenericClass<T>
     {
-        public static bool AreEqual<T>(T value1, T value2)
+        //Generic variable
+        //The data type is generic
+        private T genericMemberVariable;
+        //Generic Constructor
+        //Constructor accepts one parameter of Generic type
+        public MyGenericClass(T value)
         {
-            return value1.Equals(value2);
-        }        
+            genericMemberVariable = value;
+        }
+        //Generic Method
+        //Method accepts one Generic type Parameter
+        //Method return type also Generic
+        public T genericMethod(T genericParameter)
+        {
+            Console.WriteLine("Parameter type: {0}, value: {1}", typeof(T).ToString(), genericParameter);
+            Console.WriteLine("Return type: {0}, value: {1}", typeof(T).ToString(), genericMemberVariable);
+            return genericMemberVariable;
+        }
+        //Generic Property
+        //The data type is generic
+        public T genericProperty { get; set; }
     }
 }
